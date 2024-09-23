330585089cc92d1be7e10fe52cb45d5a22d28354
Rakesh <airrakeshkumarsharma@gmail.com>
2024-09-23 22:15:46 +0530

    Routes

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require('express')
 const app = express()
-const port = 74
+const port = 3000
 
-app.get('/google', (req, res) => {
-    // Database 
-    // Verify the user
-    // allow the user to access the google page
 
-    res.send({
-        message: 'Hello Google!',
-        name: "hello google"
-    })
+/**
+ * 1. Routes
+ * 2. Middleware
+ */
+
+app.get("/signup", (req, res) => { // localhost:3000/signup
+  // Developer 
+  res.send("Sign Up Page")
+})
+
+app.get("/login", (req, res) => { // localhost:3000/login
+  // Developer
+  res.send("Login Page")
+})
+
+app.get("/addition-calculator", (req, res) => { // localhost:3000/addition-calculator
+  // Developer
+  const a = 10
+  const b = 20
+  const sum = a + b
+  res.send(sum.toString())
+})
+
+app.get("/query-param-example", (req, res) => { // localhost:3000/query-param-example?name=John
+      // req -> take data from client
+      // res -> send data to client
+
+      console.log(req.query) // taking data via query params
+      const query = req.query
+
+      res.send(query)
+})
+
+app.get("/path-param-example/:example/:example2", (req, res) => {
+  const path = req.params // taking data via path params
+
+  res.send(path)
 })
 
 app.listen(port, () => {
